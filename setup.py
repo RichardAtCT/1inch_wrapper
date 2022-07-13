a996e81542b534be80f6fea7ee8a60840d193037
RichardAtCT <richardatk01@gmail.com>
2022-07-13 13:37:43 +0400

    update LICENCE.txt

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='1inch_wrapper',
+    version='1.0',
+    packages=['oneinch_wrapper'],
+    url='https://github.com/RichardAtCT/1inch_wrapper',
+    license='MIT',
+    author='RichardAt',
+    author_email='richardatk01@gmail.com',
+    description='a Python wrapper for the 1inch API'
+)
